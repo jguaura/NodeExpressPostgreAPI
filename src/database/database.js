8078241a049291878f0eb8bda3ef0a1973416c80
Jose Guaura <jguaura@8amarketing.com>
2020-10-12 02:12:59 -0300

    added remote DB creds

--- src/database/database.js
+++ src/database/database.js
@@ -1,25 +1,42 @@
 import Sequelize from 'sequelize'
 require('dotenv').config()
 
+//! local DB
+// const sequelize = new Sequelize(
+//     process.env.DB,
+//     process.env.NAME,
+//     process.env.PW,
+//     {
+//         host: process.env.HOST,
+//         dialect: 'postgres',
+//         pool: {
+//             max: 5,
+//             min: 0,
+//             require: 30000,
+//             idle: 10000
+//         },
+//         logging: false
+//     }
+// )
+
+//! Heroku DB
 const sequelize = new Sequelize(
-    process.env.DB,
-    process.env.NAME,
-    process.env.PW,
+    process.env.HEROKU_DB,
+    process.env.HEROKU_NAME,
+    process.env.HEROKU_PW,
     {
-        host: process.env.HOST,
+        host: process.env.HEROKU_HOST,
         dialect: 'postgres',
+        dialectOptions: {ssl: {rejectUnauthorized: false}},
         pool: {
             max: 5,
             min: 0,
             require: 30000,
-            idle: 10000
+            idle: 10
         },
-        logging: false
     }
 )
 
-console.log(process.env.DB)
-console.log(process.env.NAME)
-console.log(process.env.PW)
+
 
 export default sequelize
